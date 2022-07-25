2761778644f5a105cde5877acb285995bf7b1331
jeremy <jearonjammers@gmail.com>
2022-07-25 15:05:57 -0700

    add person

--- src/game/Game.hx
+++ src/game/Game.hx
@@ -19,14 +19,20 @@ class Game extends Component {
 
 	public function init(pack:AssetPack, width:Int, height:Int) {
 		this._root = new Entity();
+		this._thirstyPerson = new Entity().add(new ThirstyPerson(width, height));
 		this._barTable = new Entity().add(new BarTable(width, height));
 		this._meterTime = new Entity().add(new Meter(20, 40));
 		this._meterDrink = new Entity().add(new Meter(width - 120, 40).setFill(.25));
-		this._root.addChild(this._barTable).addChild(this._meterTime).addChild(this._meterDrink);
+		this._root //
+			.addChild(this._thirstyPerson) //
+			.addChild(this._barTable) //
+			.addChild(this._meterTime) //
+			.addChild(this._meterDrink); //
 	}
 
 	private var _root:Entity;
 	private var _meterTime:Entity;
 	private var _meterDrink:Entity;
 	private var _barTable:Entity;
+	private var _thirstyPerson:Entity;
 }
---
+++ src/game/ThirstyPerson.hx
@@ -0,0 +1,81 @@
+package game;
+
+import flambe.display.FillSprite;
+import flambe.display.Sprite;
+import flambe.Entity;
+import flambe.Component;
+
+class ThirstyPerson extends Component {
+	public function new(width:Int, height:Int) {
+		this.init(width, height);
+	}
+
+	override function onAdded() {
+		owner.addChild(this._root);
+	}
+
+	override function onRemoved() {
+		owner.removeChild(this._root);
+	}
+
+	public function init(width:Int, height:Int) {
+		this._root = new Entity().add(new Sprite().setXY(width / 2, height - 350));
+
+		this._torso = new Entity().add(new ThirstyPersonTorso());
+		this._head = new Entity().add(new ThirstyPersonHead());
+
+		this._root //
+			.addChild(this._torso) //
+			.addChild(this._head);
+	}
+
+	private var _root:Entity;
+	private var _head:Entity;
+	private var _torso:Entity;
+}
+
+class ThirstyPersonHead extends Component {
+	public function new() {
+		this.init();
+	}
+
+	override function onAdded() {
+		owner.addChild(this._root);
+	}
+
+	override function onRemoved() {
+		owner.removeChild(this._root);
+	}
+
+	public function init() {
+		this._root = new Entity().add(new Sprite());
+		this._head = new Entity().add(new FillSprite(0xfffff0, 210, 210).setXY(0, -190).centerAnchor());
+		this._root.addChild(this._head);
+	}
+
+	private var _root:Entity;
+	private var _head:Entity;
+}
+
+class ThirstyPersonTorso extends Component {
+	public function new() {
+		this.init();
+	}
+
+	override function onAdded() {
+		owner.addChild(this._root);
+	}
+
+	override function onRemoved() {
+		owner.removeChild(this._root);
+	}
+
+	public function init() {
+		this._root = new Entity().add(new Sprite());
+		this._torso = new Entity().add(new FillSprite(0x00ff00, 300, 300).centerAnchor());
+		this._root.addChild(this._torso);
+	}
+
+	private var _root:Entity;
+	private var _torso:Entity;
+}
